ee24ba7111528676d5b7fb0637afbc0efed56470
Amit Dev <amitdev@gmail.com>
2014-02-20 19:32:45 +0530

    Adapted to scala collections API

--- src/main/scala/ds/PriorityQueue.scala
+++ src/main/scala/ds/PriorityQueue.scala
@@ -1,46 +1,58 @@
 package ds
 
-sealed abstract class PriorityQueue[+T : Orderable] {
-  def insert[U >: T : Orderable](x: U) : PriorityQueue[U]
-  def deleteMin() : PriorityQueue[T]
-  def findMin : T
-  def isEmpty: Boolean
+import scala.collection.mutable.{ArrayBuffer, Builder}
+import scala.collection.generic.{CanBuildFrom, OrderedTraversableFactory, GenericOrderedCompanion, GenericOrderedTraversableTemplate}
+import scala.collection._
+import scala.Iterator
+
+case class Node[A](v: A, rank: Int = 0, children: List[Node[A]] = Nil)(implicit val ord: Ordering[A]) {
+  def link(other: Node[A]) =
+    if (ord.compare(v, other.v) < 0) Node(v, rank+1, other :: children)
+    else Node(other.v, other.rank+1, this :: other.children)
+
+  def toList : List[A] = v :: children.flatMap(_.toList)
 }
 
-object PriorityQueue {
+final case class PriorityQueue[T](private val nodes: List[Node[T]])(implicit val ord: Ordering[T])
+  extends Iterable[T]
+    with GenericOrderedTraversableTemplate[T, PriorityQueue]
+    with IterableLike[T, PriorityQueue[T]] {
 
-  case class Node[+T : Orderable](v: T, rank: Int = 0, children: List[Node[T]] = Nil) {
-    def link[U >: T : Orderable](other: Node[U]) =
-      if (v < other.v) Node(v, rank+1, other :: children)
-      else Node(other.v, other.rank+1, this :: other.children)
+  import PriorityQueue._
+
+  def +(x: T) : PriorityQueue[T] = PriorityQueue(insertNode(Node(x), nodes))
+  def findMin: T = getMinNode.v
+
+  def deleteMin(): PriorityQueue[T] = {
+    val minNode = getMinNode
+    PriorityQueue(meldLists(nodes.filter(_ != minNode), minNode.children.reverse))
   }
 
-  case class BinomialQueue[+T : Orderable](nodes: List[Node[T]]) extends PriorityQueue[T] {
-    def insert[U >: T : Orderable](x: U) : BinomialQueue[U] = BinomialQueue(insertNode(Node(x), nodes))
-    override def findMin: T = getMinNode.v
-
-    override def deleteMin(): PriorityQueue[T] = {
-      val minNode = getMinNode
-      BinomialQueue(meldLists(nodes.filter(_ != minNode), minNode.children.reverse))
-    }
-
-    def meld[U >: T : Orderable](that: BinomialQueue[U]) : BinomialQueue[U] = (this, that) match {
-      case (BinomialQueue(Nil), q) => q
-      case (q, BinomialQueue(Nil)) => q
-      case (BinomialQueue(thisList), BinomialQueue(thatList)) => BinomialQueue(meldLists(thisList, thatList))
-    }
-
-    private def getMinNode : Node[T] = {
-      lazy val cmp: Ordering[Node[T]] = new Ordering[Node[T]] {
-        override def compare(x: Node[T], y: Node[T]): Int = x.v compare y.v
-      }
-      nodes.min(cmp)
-    }
-
-    override def isEmpty: Boolean = nodes.isEmpty
+  def meld(that: PriorityQueue[T]) : PriorityQueue[T] = (this, that) match {
+    case (PriorityQueue(Nil), q) => q
+    case (q, PriorityQueue(Nil)) => q
+    case (PriorityQueue(thisList), PriorityQueue(thatList)) => PriorityQueue(meldLists(thisList, thatList))
   }
 
-  private def meldLists[T : Orderable](q1: List[Node[T]], q2: List[Node[T]]) : List[Node[T]] = (q1, q2) match {
+  private def getMinNode : Node[T] = {
+    lazy val cmp: Ordering[Node[T]] = new Ordering[Node[T]] {
+      override def compare(x: Node[T], y: Node[T]): Int = ord.compare(x.v, y.v)
+    }
+    nodes.min(cmp)
+  }
+
+  override def isEmpty: Boolean = nodes.isEmpty
+
+  override def orderedCompanion: GenericOrderedCompanion[PriorityQueue] = PriorityQueue
+
+  override def iterator: Iterator[T] = nodes.flatMap(_.toList).iterator
+
+  override protected[this] def newBuilder: mutable.Builder[T, PriorityQueue[T]] = PriorityQueue.newBuilder
+}
+
+object PriorityQueue extends OrderedTraversableFactory[PriorityQueue] {
+
+  private def meldLists[T](q1: List[Node[T]], q2: List[Node[T]]) : List[Node[T]] = (q1, q2) match {
     case (Nil, q) => q
     case (q, Nil) => q
     case (x :: xs, y :: ys) => if (x.rank < y.rank) x :: meldLists(xs, y :: ys)
@@ -48,11 +60,17 @@ object PriorityQueue {
                                else insertNode(x.link(y), meldLists(xs, ys))
   }
 
-  private def insertNode[T : Orderable](n: Node[T], lst: List[Node[T]]) : List[Node[T]] = lst match {
+  private def insertNode[T](n: Node[T], lst: List[Node[T]]) : List[Node[T]] = lst match {
     case Nil => List(n)
     case x :: xs => if (n.rank < x.rank) n :: x :: xs
     else insertNode(x.link(n), xs)
   }
 
-  def apply[T : Orderable](xs: T*): PriorityQueue[T] = xs.foldLeft(BinomialQueue[T](Nil))((t, x) => t.insert(x))
+  override def newBuilder[A](implicit ord: Ordering[A]): mutable.Builder[A, PriorityQueue[A]] = new ArrayBuffer[A] mapResult {xs =>
+    var r = new PriorityQueue[A](Nil)
+    for (x <- xs) r = r + x
+    r
+  }
+
+  implicit def canBuildFrom[A](implicit ord: Ordering[A]): CanBuildFrom[Coll, A, PriorityQueue[A]] = new GenericCanBuildFrom[A]
 }
--- src/test/scala/PriorityQueueTest.scala
+++ src/test/scala/PriorityQueueTest.scala
@@ -7,22 +7,31 @@ import org.scalatest.prop.Checkers
 @RunWith(classOf[JUnitRunner])
 class PriorityQueueTest extends FunSuite with Checkers {
 
+  val inputs = Map(List(1) -> 1,
+    List(2,1,4,5) -> 1,
+    List(4,5,6,7,8,9) -> 4)
+
   test("empty") {
-    assert(PriorityQueue().isEmpty, "Empty queue is really empty")
+    val empty = PriorityQueue(3).deleteMin()
+    assert(empty.isEmpty, "Empty queue is really empty")
+    intercept[UnsupportedOperationException] {
+      empty.findMin
+    }
+    intercept[UnsupportedOperationException] {
+      empty.deleteMin()
+    }
+    assert(empty.size == 0)
   }
 
   test("Insert") {
-    val inputs = Map(List(1) -> 1,
-                     List(2,1,4,5) -> 1,
-                     List(4,5,6,7,8,9) -> 4)
-    for ((k,v) <- inputs)
-      assert(PriorityQueue(k:_*).findMin == v, "Insert element works")
+    for ((k,v) <- inputs) {
+      val q = PriorityQueue(k: _*)
+      assert(q.findMin == v, "Insert element works")
+      assert(q.size == k.length)
+    }
   }
 
   test("Delete") {
-    val inputs = Map(List(1) -> 1,
-      List(2,1,4,5) -> 1,
-      List(4,5,6,7,8,9) -> 4)
     for ((k,v) <- inputs) {
       var pq = PriorityQueue(k:_*)
       val s = k.sorted
@@ -30,9 +39,21 @@ class PriorityQueueTest extends FunSuite with Checkers {
         val sub = s.slice(i, k.length)
         if (!sub.isEmpty) {
           pq = pq.deleteMin()
+          assert(pq.size == sub.length)
           assert(pq.findMin == sub(0), "Delete element works")
         }
       }
+    }
+  }
+
+  test("Common Operations") {
+    for ((k,v) <- inputs) {
+      val q = PriorityQueue(k: _*)
+      //Test Iteration
+      for (i <- q) assert(k.contains(i))
+      //Test map, reduce etc
+      assert((q map (_*10) reduce (_+_)) == (k map (_*10) reduce (_+_)))
+      assert(q.map(_*10).isInstanceOf[PriorityQueue[Int]])
     }
   }
 }
